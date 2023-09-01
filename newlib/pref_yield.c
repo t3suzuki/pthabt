469a5cf4048d7ddf870c17266faaed3d1b04382d
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-09-01 18:35:02 +0000

    complete pref_yield.c

--- newlib/pref_yield.c
+++ newlib/pref_yield.c
@@ -1,14 +1,28 @@
 #include <stdint.h>
+#define LIBDL
 #include "e9patch/examples/stdlib.c"
 
-void
-sched_yield()
-{
-}
+static int (*dl_ABT_thread_yield)(void) = NULL;
 
 void pref_yield(int64_t a)
 {
-  //printf("prefetching... %p\n", (void *)a);
-  __builtin_prefetch(a);
-  //sched_yield();
+  __builtin_prefetch((void *)a);
+  dl_ABT_thread_yield();
+}
+
+void init(int argc, char **argv, char **envp, void *dynamic)
+{
+  dlinit(dynamic);
+  
+  void *dl_handle = dlopen("libabt.so.1", RTLD_NOW);
+  if (dl_handle == NULL) {
+    printf("dlopen error\n");
+    exit(1);
+  }
+  
+  dl_ABT_thread_yield = dlsym(dl_handle, "ABT_thread_yield");
+  if (dl_ABT_thread_yield == NULL) {
+    printf("dlsym error\n");
+    exit(1);
+  }
 }
