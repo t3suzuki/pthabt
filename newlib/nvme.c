9e9f19659aac546c652d1b397ca461a0ec41dd71
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-09-11 23:17:47 +0000

    nvme lib

--- newlib/nvme.c
+++ newlib/nvme.c
@@ -21,6 +21,28 @@ static int enable_bus_master(int uio_index)
   return 0;
 }
 
+static size_t v2p(size_t vaddr) {
+  FILE *pagemap;
+  size_t paddr = 0;
+  ssize_t offset = (vaddr / sysconf(_SC_PAGESIZE)) * sizeof(uint64_t);
+  uint64_t e;
+
+  if ((pagemap = fopen("/proc/self/pagemap", "r"))) {
+    if (lseek(fileno(pagemap), offset, SEEK_SET) == offset) {
+      if (fread(&e, sizeof(uint64_t), 1, pagemap)) {
+	if (e & (1ULL << 63)) {
+	  paddr = e & ((1ULL << 55) - 1);
+	  paddr = paddr * sysconf(_SC_PAGESIZE);
+	  paddr = paddr | (vaddr & (sysconf(_SC_PAGESIZE) - 1));
+	}
+      }
+    }
+    fclose(pagemap);
+  }
+  return paddr;
+}
+
+
 int
 init()
 {
@@ -35,7 +57,8 @@ init()
     perror("open");
     return -1;
   }
-  volatile uint32_t *regs32 = mmap(0, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  volatile uint32_t *regs32 = mmap(0, 0x4000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  volatile uint64_t *regs64 = (volatile uint64_t *)regs32;
   if (regs32 == MAP_FAILED) {
     perror("mmap");
     return -1;
@@ -51,6 +74,15 @@ init()
   printf("%lx\n", csts);
   
   assert(csts == 0);
+
+  const int sz = 2*1024*1024;
+  void *admin_queue = mmap(NULL, sz, PROT_READ | PROT_WRITE,
+			   MAP_PRIVATE | MAP_HUGETLB | MAP_ANONYMOUS | MAP_POPULATE, 0, 0);
+  bzero(admin_queue, sz);
+
+  regs64[0x30 / sizeof(uint64_t)] = v2p(admin_queue); // CQ
+  regs64[0x28 / sizeof(uint64_t)] = v2p(admin_queue + 0x1000); // SQ
+
   
 }
 
