b883f506aa38cd3df93a295aba5f245f79aca3e5
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-10-04 05:08:54 +0000

    up

--- newlib/readetest.c
+++ newlib/readetest.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define FILESZ (1024*1024*512)
+
 typedef struct {
   int fd;
   int tid;
@@ -16,26 +18,21 @@ void worker(void *a)
   arg_t *arg = (arg_t *)a;
   int fd = arg->fd;
   int tid = arg->tid;
-  char *wbuf = aligned_alloc(4096, 512);
   char *rbuf = aligned_alloc(4096, 512);
   int i;
   int n;
-#if 1
-  for (i=0; i<128; i++) {
-    wbuf[i] = i+0x7+tid;
-  }
-  n = pwrite64(fd, wbuf, 512, tid*512);
-#endif
 
   
   for (i=0; i<128; i++) {
     rbuf[i] = 0;
   }
-  n = pread64(fd, rbuf, 512, tid*512);
-  printf("tid = %d %d %d\n", tid, wbuf[0], rbuf[0]);
+  int r = rand() % (FILESZ / 512);
+  n = pread64(fd, rbuf, 512, r*512);
+  //printf("tid = %d %d %d\n", tid, wbuf[0], rbuf[0]);
+  printf("tid = %d %d %d\n", tid, (char)r, rbuf[0]);
 }
 
-#define N_TH (2)
+#define N_TH (1)
 
 int
 main()
@@ -43,9 +40,22 @@ main()
   int fd = open("myfile", O_RDWR|O_DIRECT);
   printf("fd = %d\n", fd);
   int i;
+  char *wbuf = aligned_alloc(4096, 512);
   pthread_t pth[N_TH];
   arg_t arg[N_TH];
+  int j;
 
+#if 1
+  printf("writing...\n");
+  for (j=0;j<(FILESZ/512);j++) {
+    wbuf[0] = j;
+    pwrite64(fd, wbuf, 512, j*512);
+    if (j % 16384 == 0)
+      printf("readetest write lba %d\n", j);
+  }
+  printf("write done\n");
+#endif
+  
   for (i=0; i<N_TH; i++) {
     arg[i].tid = i;
     arg[i].fd = fd;
